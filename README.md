db10c72f98117150ba699aaa8196f7ab272566c9
SamPM092 <sammich4294@gmail.com>
2026-06-18 17:52:51 +0700

    learn: chapter 1-9 (20%)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+Cover this: https://nextjs.org/learn/pages-router/
---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+import styles from '../../components/layout.module.css';
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <h1>First Post</h1>;
+            <h2> <Link href="/" className={styles.link}>Back</Link></h2>
+        </Layout>
+    )
+
+}
