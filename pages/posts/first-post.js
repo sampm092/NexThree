f79c3449f664d23ffbe772f0582c0e395e6e952f
SamPM092 <sammich4294@gmail.com>
2026-06-23 16:24:14 +0700

    Refactor code structure for improved readability and maintainability

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -6,8 +6,12 @@ import styles from '../../components/layout.module.css';
 export default function FirstPost() {
     return (
         <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>;
             <h2> <Link href="/" className={styles.link}>Back</Link></h2>
+            
         </Layout>
     )
 
